8c1f01ffdd12c1c2a34e99e8f92cd10b36095cfd
Johnson1255 <johnson.chen@upb.edu.co>
2024-02-13 12:34:59 -0500

    Proyecto Base Adelantado

---
+++ app/src/main/java/com/upb/cartavirtual/PlatosFuertesActivity.java
@@ -0,0 +1,14 @@
+package com.upb.cartavirtual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlatosFuertesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_platos_fuertes);
+    }
+}
